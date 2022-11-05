ad1f62161cb2abadb34c4d0104ef5ce784f332c9
taylor <taylor.viars@yahoo.com>
2022-11-05 19:58:23 -0400

    commit tree c7a4227ed5b048d946905b75ec4cb59f5b38600a

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'andrew'
+
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'jonnel'
+}
